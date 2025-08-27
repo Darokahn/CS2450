04f1f6a0346f8a24fd1cfdaf02b77aec748d719b
Andrew <eardley.andrew1@gmail.com>
2025-08-27 10:40:50 -0600

    warn user when only one guess remains

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ name = input("first, your name: ")
 guessCount = 0
 while (result != 'y' and guessCount < 5):
     guess = random.randint(0, 99)
-    result = input(f"is {guess} your age? ")
+    if (guessCount == 4):
+        print("This is my last guess.")
+    result = input(f"Is {guess} your age? ")
     if (result != 'y'):
         print("Rats.")
         guessCount += 1
 if guessCount == 5:
-    print("shoot. I ran out of guesses.")
+    print("Shoot. I ran out of guesses.")
 else:
     print(f"{name} is {guess} years old.")
