5ee735d20555f4225214e05d8a023f083ef57c74
Andrew <eardley.andrew1@gmail.com>
2025-08-27 10:36:43 -0600

    add guess tracker

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import random
 
-print("Hello! I am going to guess your age. Okay?")
+print("Hello! I am going to guess your age. I'm warning you, I'm pretty good...")
 
 result = None
 
 name = input("first, your name: ")
-while (result != 'y'):
-    guess = random.randint(15, 30)
+guessCount = 0
+while (result != 'y' and guessCount < 5):
+    guess = random.randint(0, 99)
     result = input(f"is {guess} your age? ")
     if (result != 'y'):
         print("Rats.")
-print(f"{name} is {guess} years old.")
+        guessCount += 1
+if guessCount == 5:
+    print("shoot. I ran out of guesses.")
+else:
+    print(f"{name} is {guess} years old.")
